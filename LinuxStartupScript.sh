ef8bd8d35b2dcd29ead24266af24a8afc90bd77f
Dayo <1690328478@qq.com>
2026-01-18 22:38:20 +0800

    Update LinuxStartupScript.sh

--- LinuxStartupScript.sh
+++ LinuxStartupScript.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # For Debian and Ubuntu vultr StartupScript
 
+# YOUR_WEBHOOK_TOKEN
+# https://open.feishu.cn/open-apis/bot/v2/hook/
+YOUR_WEBHOOK_TOKEN = "https://open.feishu.cn/open-apis/bot/v2/hook/xxxxxxxxxxxxx"
 # disable firewall
 ufw disable
 
@@ -12,6 +15,17 @@ sudo sh get-docker.sh
 apt-get update
 apt-get install docker-compose-plugin
 
-# xray docker
+# install xray docker
 EXTERNAL_PORT=2333 && docker run -d --name xray_reality --restart=always --log-opt max-size=100m --log-opt max-file=3 -p $EXTERNAL_PORT:443 -e EXTERNAL_PORT=$EXTERNAL_PORT wulabing/xray_docker_reality:latest && sleep 3 && docker exec -it xray_reality cat /config_info.txt
 
+curl -s -X POST "$YOUR_WEBHOOK_TOKEN" \
+  -H "Content-Type: application/json" \
+  -d '{"msg_type":"text","content":{"text":"Xray Reality VLESS link:\n"}}'
+  
+docker logs xray_reality 2>&1 \
+| grep -o 'vless://[^ ]*-wulabing_docker_vless_reality_vision' \
+| head -n 1 \
+| xargs -I {} curl -s -X POST "$YOUR_WEBHOOK_TOKEN" \
+  -H "Content-Type: application/json" \
+  -d '{"msg_type":"text","content":{"text":"{}"}}'
+  
